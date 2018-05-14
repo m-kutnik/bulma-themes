7c83029a81e8bd0330e311375759cd0e57dfdc02
Mike <gimp2312@gmail.com>
2018-05-14 14:33:21 +0200

    reorder imports, add 'simplebar'

--- src/main.js
+++ src/main.js
@@ -2,17 +2,21 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
-import Tab from "./components/Tab"
-import Tabs from "./components/Tabs"
-import { Chrome } from "vue-color"
 import ColorPicker from "./components/ColorPicker"
+import Preview from "./components/Preview"
+import Tabs from "./components/Tabs"
+import Tab from "./components/Tab"
+import { Chrome } from "vue-color"
+import "simplebar/dist/simplebar.css"
 import "./registerServiceWorker"
+import "simplebar"
 import "photon"
 
 Vue.config.productionTip = false
 
 Vue.component("colorPicker", ColorPicker)
 Vue.component("chromePicker", Chrome)
+Vue.component("preview", Preview)
 Vue.component("tabs", Tabs)
 Vue.component("tab", Tab)
 
